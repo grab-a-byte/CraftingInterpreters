1af78428e08dea24512e9ebc3ece7ba2627cc460
Adam Parker <adamparker@pop-os.localdomain>
2020-08-09 13:51:06 +0100

    Starting initial Implementation

---
+++ TreeWalking/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+open System.IO
+
+let run (sourceCode:string) =
+    printfn "run"
+
+let runPrompt () =
+    Console.R
+
+let runFile path =
+    File.ReadAllText path
+    |> run
+    printfn "runFile"
+
+[<EntryPoint>]
+let main argv =
+
+    if argv.Length > 1 then
+        printfn "Usage: flox [script]"
+        exit(64)
+    else if argv.Length = 1 then
+        runFile(argv.[0])
+    else
+        runPrompt()
+    0
